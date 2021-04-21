84f57bb9e80ead084dc001f02e084df64691de0a
Liam DeVoe <orionldevoe@gmail.com>
2021-04-20 23:57:06 -0400

    add dining dollars amount to examples

--- examples.py
+++ examples.py
@@ -2,4 +2,6 @@ from umdauth import UMDAuth
 from config import username, password, auth_cookies, identity_jsession_id
 
 auth = UMDAuth(username, password, auth_cookies, identity_jsession_id)
-auth.send_daily_symptom_survey()
+# auth.send_daily_symptom_survey()
+dd = auth.get_dining_dollars()
+print(dd.total_amount, dd.current_amount, dd.rollover_amount)
