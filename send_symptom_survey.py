f9375856d02ffb88ed3457e5516b0114558bade8
Liam DeVoe <orionldevoe@gmail.com>
2021-04-20 23:35:20 -0400

    use config file in script

--- send_symptom_survey.py
+++ send_symptom_survey.py
@@ -1,4 +1,5 @@
 from umdauth import UMDAuth
+from config import username, password, auth_cookies, identity_jsession_id
 
-auth = UMDAuth()
+auth = UMDAuth(username, password, auth_cookies, identity_jsession_id)
 auth.send_daily_symptom_survey()
